4a9bf6931b0685e65c0b1075bf1a65c7abf5af56
soumen-github-account <soumendas7584@gmail.com>
2026-03-22 22:07:59 +0530

    pwa

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,5 +4,32 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(), 
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'logo192.png', 'logo512.png'],
+      manifest: {
+        name: 'Payflow',
+        short_name: 'Payflow',
+        description: 'Pay easy and get reward',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'logo192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'logo512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
